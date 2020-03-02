fcc709ad65b5bc1ea5ca7d883745ef979ca2c116
Sonyxe <61245381+Sonyxe@users.noreply.github.com>
2020-03-02 11:22:53 -0500

    Add files via upload

---
+++ namudarbas.js
@@ -0,0 +1,44 @@
+let num=3;
+alert(num);
+let a=10;
+let b=2;
+document.write(a+b);
+document.write('<br>');
+document.write(a-b);
+document.write('<br>');
+document.write(a*b);
+document.write('<br>');
+document.write(a/b);
+document.write('<br>');
+let c=15;
+let d=2;
+let result=c+d;
+document.write(result);
+document.write('<br>');
+let str='hello world!'
+document.write(str);
+document.write('<br>');
+let str1='hello'
+let str2='world'
+document.write(str1 + str2);
+document.write('<br>');
+let name=('Sonata');
+document.write('hello' + name);
+document.write('<br>');
+let age=18
+document.write('Im' +age + 'old');
+document.write('<br>');
+let username=prompt('ivesti prisijungimo varda')
+alert('sveiki atvyke');
+
+
+
+
+
+
+
+
+
+
+
+
